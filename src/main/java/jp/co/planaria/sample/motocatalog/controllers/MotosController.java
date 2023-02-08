2f9cda9be56dd09ca23793f48e2b729ce5d7a1aa
kawashima nagisa <kawashima.it333@outlook.com>
2023-02-08 16:01:23 +0900

    Edit

---
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -0,0 +1,19 @@
+package jp.co.planaria.sample.motocatalog.controllers;
+
+import java.security.KeyStore.Entry.Attribute;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MotosController {
+    
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name, Model model) {
+    
+        model.addAttribute("name", name);
+        return "test";
+    }
+}
